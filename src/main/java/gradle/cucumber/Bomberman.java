bdffe91a1a330c2b14bef228694365e20d0b4590
Juan Escalada <JuanEscalada@github.com>
2019-05-06 05:20:26 -0300

    Se agrega comportamiento de explosion y escenario para punto 2.i funcionando

--- src/main/java/gradle/cucumber/Bomberman.java
+++ src/main/java/gradle/cucumber/Bomberman.java
@@ -4,7 +4,6 @@ public class Bomberman extends Contenido{
 
     //Por ahora a Bomberman le importa saber donde esta.
     public EstadoDelPersonaje estado;
-    public Bomba bomba;
 
     public Bomberman(){
         this.estado = new Vivo();
@@ -20,4 +19,9 @@ public class Bomberman extends Contenido{
     public Boolean esSolido() { return true; }
     @Override
     public void interactuarConPersonajeMoviendoseA(Juego juego, Posicion destino){ /*Se deja el metodo por compatibilidad por ahora pero no se deberia dar nunca este caso.*/}
+    @Override
+    public Contenido interactuarConBomba(){
+        this.morir();
+        return this;
+    }
 }
--- src/main/java/gradle/cucumber/Contenido.java
+++ src/main/java/gradle/cucumber/Contenido.java
@@ -3,6 +3,7 @@ package gradle.cucumber;
 public abstract class Contenido {
 
     public abstract void interactuarConPersonajeMoviendoseA(Juego juego, Posicion destino);
+    public abstract Contenido interactuarConBomba();
     public abstract Boolean esSolido();
     public void tick(Juego juego){/*Por default no hacemos nada.*/};
 }
